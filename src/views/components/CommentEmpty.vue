647af2a47f546c570cbfe3e6602ead9718beb99d
wushaojie <wushaojie@live.com>
2016-09-11 22:09:49 +0800

    修改评论样式。

--- src/views/components/CommentEmpty.vue
+++ src/views/components/CommentEmpty.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="comment-empty" v-if="commentCount === 0 && comments.length === 0">
+  <div class="comment-empty" v-if="commentCount === 0 && addComments.length === 0">
     <i class="icon icon-pen"></i>
     <p class="comment-empty-text">智慧如你，不如<a href=""
                                             @touchstart.prevent="onWrite">发表一点想法</a>吧！
@@ -9,7 +9,7 @@
 
 <script>
   export default {
-    props: ['commentCount', 'comments'],
+    props: ['commentCount', 'addComments'],
     methods: {
       onWrite() {
         this.$dispatch('onWrite');
