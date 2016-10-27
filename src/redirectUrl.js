1de36aeb727b10937389a90b3ece45b23ea688a3
wushaojie <wushaojie@live.com>
2016-10-27 19:50:10 +0800

    bug fixed.

--- src/redirectUrl.js
+++ src/redirectUrl.js
@@ -2,14 +2,15 @@ import config from './config';
 
 export default function redirectUrl(url) {
   const isUrl = typeof url === 'string';
+  const isReplace = typeof url === 'boolean';
   const redirectUrl = decodeURIComponent(isUrl ? url : location.search.substring(13).split('&')[0]);
   const whiteListUrls = config.whiteListUrls;
   for (let i = 0; i < whiteListUrls.length; i++) {
     if (whiteListUrls[i].test(redirectUrl)) {
-      if (isUrl) {
-        location.href = redirectUrl;
-      } else {
+      if (isReplace) {
         location.replace(redirectUrl);
+      } else {
+        location.href = redirectUrl;
       }
       return;
     }
