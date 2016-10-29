f7b051985abd3dd66457fe7bb84cf4d5f20d237d
wushaojie <wushaojie@live.com>
2016-10-29 15:45:23 +0800

    bug fixed.

--- static/js/getDeviceId.js
+++ static/js/getDeviceId.js
@@ -6,10 +6,26 @@ function uuid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
 function getDeviceId() {
+  var isOverseaApp = navigator.userAgent.toLowerCase().indexOf('oversea_') != -1;
   var deviceId = localStorage.getItem('deviceId');
   if (!deviceId) {
-    deviceId = uuid();
+    if (isOverseaApp) {
+      deviceId = getParameterByName('deviceId');
+    } else {
+      deviceId = uuid();
+    }
     localStorage.setItem('deviceId', deviceId);
   }
   return deviceId;
--- static/js/vendor/jquery/jquery.encrypt.js
+++ static/js/vendor/jquery/jquery.encrypt.js
@@ -1,11 +1,7 @@
 (function($) {
   $.extend($.ajaxSettings, {
     beforeSend: function(xhr) {
-      var token = $.cookie('token');
       var deviceId = getDeviceId();
-      if (token) {
-        xhr.setRequestHeader('token', token);
-      }
       xhr.setRequestHeader('deviceId', deviceId);
     }
   });
